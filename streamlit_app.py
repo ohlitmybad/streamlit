dd24a47d291a4026379ab8ee8388a53468f053a1
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2023-10-26 01:30:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,12 @@ import os
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
+from datetime import datetime
 
 # Define the path to the users.txt file
 USERS_FILE = 'users.txt'
 
-# Define a file to store user query counts
+# Define a file to store user query counts and timestamps
 QUERY_COUNT_FILE = 'query_counts.txt'
 
 # Function to check user credentials
@@ -20,29 +21,25 @@ def authenticate_user(username, password):
                 return True
     return False
 
+# Function to load and update query counts
+def update_query_counts(query_counts):
+    current_time = datetime.now()
+    for username, data in list(query_counts.items()):
+        # Check if the data is within the current rolling month
+        first_query_time = data['first_query']
+        if current_time - first_query_time < timedelta(days=30):
+            # Still within the rolling month, keep the data
+            pass
+        else:
+            # It's a new rolling month, remove the old data
+            query_counts.pop(username)
+    return query_counts
 
-def load_query_counts():
-    if os.path.exists(QUERY_COUNT_FILE):
-        with open(QUERY_COUNT_FILE, 'r') as count_file:
-            query_counts = {}
-            for line in count_file:
-                parts = line.strip().split(':')
-                if len(parts) == 2:
-                    user, count = parts
-                    query_counts[user] = int(count)
-            return query_counts
-    return {}
-
-
-# Function to save user query counts
-def save_query_counts(query_counts):
-    with open(QUERY_COUNT_FILE, 'w') as count_file:
-        for user, count in query_counts.items():
-            count_file.write(f"{user}:{count}\n")
-
-# Function to check if a user has reached their query limit
-def is_query_limit_reached(username, query_counts, limit=3):
-    return query_counts.get(username, 0) >= limit
+# Function to check if a user has reached their rolling monthly query limit
+def is_query_limit_reached(username, query_counts, limit=1000):
+    if username in query_counts:
+        return query_counts[username]['count'] >= limit
+    return False
 
 st.set_page_config(page_title='DataMB Chat ⚽')
 st.title('DataMB Chat ⚽')
@@ -67,16 +64,22 @@ username = st.text_input('Username:')
 password = st.text_input('Password:', type="password")
 query_text = st.text_input('Enter your query:', placeholder='Enter query here ...')
 
-query_counts = load_query_counts()
+query_counts = update_query_counts(query_counts)
 
 if authenticate_user(username, password):
     if not is_query_limit_reached(username, query_counts):
         st.header('Output')
         generate_response(query_text)
         # Update the query count for the user
-        query_counts[username] = query_counts.get(username, 0) + 1
-        save_query_counts(query_counts)
+        current_time = datetime.now()
+        if username in query_counts:
+            query_counts[username]['count'] += 1
+        else:
+            query_counts[username] = {'count': 1, 'first_query': current_time}
+        with open(QUERY_COUNT_FILE, 'w') as count_file:
+            for user, data in query_counts.items():
+                count_file.write(f"{user}:{data['count']}:{data['first_query']}\n")
     else:
-        st.error('Query limit (10 queries per day) reached for this user.')
+        st.error('Query limit (1000 queries per rolling month) reached for this user.')
 else:
     st.error('Authentication failed. Please check your credentials.')
