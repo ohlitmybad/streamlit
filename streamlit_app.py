7f5b438bd76396494c69175e3f9599642b9977f6
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2023-10-25 22:35:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-import pandas as pd
 import sqlite3
-from datetime import datetime
-import threading
+import pandas as pd
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
 
 st.set_page_config(page_title='DataMB Chat ⚽')
 st.title('DataMB Chat ⚽')
@@ -11,54 +12,47 @@ def load_csv():
     df = pd.read_csv("data.csv")
     return df
 
-def generate_response(input_query):
-    user_id = st.session_state.user_id
-    current_date = datetime.now().strftime('%Y-%m-%d')
+def authenticate_user(username, password):
+    # Connect to the users database
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
 
-    conn = sqlite3.connect('user_activity.db')
-    c = conn.cursor()
+    # Check if the provided username and password match any user in the database
+    cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+    user = cursor.fetchone()
 
-    # Retrieve user activity data from the database
-    c.execute('SELECT query_count, last_query_date FROM user_activity WHERE user_id=?', (user_id,))
-    user_data = c.fetchone()
+    conn.close()
+    return user
 
-    if user_data:
-        query_count, last_query_date = user_data
-        if last_query_date != current_date:
-            # Reset the query count if it's a new day
-            query_count = 0
+def generate_response(input_query, username):
+    # Check if the user is authenticated
+    user = authenticate_user(username, input_query)
 
-        if query_count >= 3:
-            st.error("You've reached your daily query limit (3 queries per day).")
-            return
-
-    # Create Pandas DataFrame Agent and perform the query
+    if user is None:
+        return st.error("Authentication failed. Please check your credentials.")
+    
     llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0, openai_api_key=openai_api_key)
     df = load_csv()
+    # Create Pandas DataFrame Agent
     agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
+    # Perform Query using the Agent
     response = agent.run(input_query)
-
-    # Update the user activity data in the database
-    if user_data:
-        query_count += 1
-        c.execute('UPDATE user_activity SET query_count=?, last_query_date=? WHERE user_id=?',
-                  (query_count, current_date, user_id))
-    else:
-        c.execute('INSERT INTO user_activity (user_id, query_count, last_query_date) VALUES (?, ?, ?)',
-                  (user_id, 1, current_date))
-
-    conn.commit()
-    conn.close()
-    st.success(response)
+    return st.success(response)
 
 OPAK_KEY = "QOxvASrYaXeRFFHgajIdT3BlbkFJkQ37OFVOZVOc8t07WJI5"
 openai_api_key = "sk-" + OPAK_KEY
 
-if 'user_id' not in st.session_state:
-    # Generate a user ID based on session information
-    st.session_state.user_id = hash(str(st.session) + str(datetime.now()))
+# User authentication
+username = st.text_input('Username:', key='username')
+password = st.text_input('Password:', key='password', type='password')
 
-query_text = st.text_input('Enter your query:', placeholder='Enter query here ...')
-
-st.header('Output')
-generate_response(query_text)
+if st.button('Log In'):
+    response = authenticate_user(username, password)
+    if response:
+        st.success("Authentication successful. You are now logged in.")
+    else:
+        st.error("Authentication failed. Please check your credentials.")
+else:
+    query_text = st.text_input('Enter your query:', placeholder='Enter query here ...')
+    st.header('Output')
+    generate_response(query_text, username)
