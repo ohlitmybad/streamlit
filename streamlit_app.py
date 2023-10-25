1c151219d64366ba9f3a4bb394d5397e55ebac4d
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2023-10-25 03:10:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def load_csv():
     return df
 
 def generate_response(input_query):
-    llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0, openai_api_key=openai_api_key)
+    llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.8, openai_api_key=openai_api_key)
     df = load_csv()
     # Create Pandas DataFrame Agent
     agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
