ec69ca74ee6d6d33c308127e19039aa4074e9a27
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2023-10-27 05:19:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,13 +82,14 @@ query_counts = load_query_counts()
 if user_exists(username):
     if not is_query_limit_reached(username, query_counts):
         st.header('Output')
-        if generate_response(query_text):
+        with st.spinner('Processing your request...'):
+            if generate_response(query_text):
     # Only increment the count if the query was successful
-            user_data = query_counts.get(username, {})
-            today = datetime.date.today().isoformat()
-            user_data[today] = user_data.get(today, 0) + 1
-            query_counts[username] = user_data
-            save_query_counts(query_counts)
+                user_data = query_counts.get(username, {})
+                today = datetime.date.today().isoformat()
+                user_data[today] = user_data.get(today, 0) + 1
+                query_counts[username] = user_data
+                save_query_counts(query_counts)
 
     else:
         st.error('Daily query limit (15) reached for this user.')
