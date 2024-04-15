cb67e6b4ee2d4440c1b732c7f4da9a0b84d99ff1
ohlitmybad <otdog96@gmail.com>
2024-04-15 08:50:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ def is_query_limit_reached(username, query_counts, limit=DAILY_QUERY_LIMIT):
 st.set_page_config(page_title='DataMB Chat ⚽')
 st.title('DataMB Chat ⚽')
 
-OpenAI_key = os.environ.get("OPEN_AI_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 
 def load_csv():
@@ -65,7 +65,7 @@ def load_csv():
     return df
 
 def generate_response(input_query):
-    llm = ChatOpenAI(model_name='gpt-3.5-turbo-0125', temperature=0, openai_api_key=OpenAI_key)
+    llm = ChatOpenAI(model_name='gpt-3.5-turbo-0125', temperature=0, openai_api_key=OPENAI_API_KEY)
     df = load_csv()
     # Create Pandas DataFrame Agent
     agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
