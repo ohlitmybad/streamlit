29d27b6d76fcaad0b30eeb73fd77bfe698985ad6
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2024-04-25 06:33:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ body {
 """, unsafe_allow_html=True)
 
 # Title and image
-st.image("https://datamb.football/logochat.png", width=50)
+st.image("https://datamb.football/logochat.png", width=50, style="margin:auto;")
 st.title('DataMB Chat ⚽')
 
 # Load CSV data
