850fe13c621ddaf3e37c1fb9f5924533301668b5
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2024-04-27 20:03:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,7 +91,7 @@ def load_csv():
 
 # Generate response
 def generate_response(input_query):
-    llm = ChatOpenAI(model_name='gpt-3.5-turbo-1106', temperature=0, openai_api_key=OPENAI_API_KEY)
+    llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0, openai_api_key=OPENAI_API_KEY)
     df = load_csv()
     agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
     input_query = input_query.replace("POSS+/-", "((Interceptions per 90 + Sliding tackles per 90 + (Defensive duels per 90 * Defensive duels won, % / 100)) * ((Passes per 90 + Offensive duels per 90)/100) - (((100 - Accurate passes, %)*(Passes per 90 / 100)+(100 - Offensive duels won, %)*(Offensive duels per 90 / 100)) * (100/(Passes per 90 + Offensive duels per 90))))")
